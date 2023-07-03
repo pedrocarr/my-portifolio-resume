e075283c104eb38c777cc8b726b0d1211d42aa3a
Pedro Carvalho <eng.pedro.amb@gmail.com>
2023-07-03 14:53:28 +0200

    include about and workexperience section

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
+import AboutSection from "@/components/AboutSection"
 import HeroSection from "@/components/HeroSection"
+import WorkExperienceSection from "@/components/WorkExperienceSection"
+
 export default function Home() {
   return (
     <main>
      <HeroSection />
+     <AboutSection />
+     <WorkExperienceSection />
     </main>
   )
 }
