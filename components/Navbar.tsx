35fc73100da57d1aa61085719a75ad23f7c8ce3a
Pedro Carvalho <eng.pedro.amb@gmail.com>
2023-07-03 14:52:39 +0200

    change navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -22,8 +22,8 @@ const NAV_ITEMS: Array<NavItem> = [
     page: "about",
   },
   {
-    label: "Projects",
-    page: "projects",
+    label: "Work Experience",
+    page: "work-experience",
   },
 ]
 
