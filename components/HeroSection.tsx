4c2bb57ac10934471ba642aa7df9c9230bdccf76
Pedro Carvalho <eng.pedro.amb@gmail.com>
2023-12-31 12:18:15 +0100

    add ruby and rails skills

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -38,13 +38,13 @@ const HeroSection = () => {
         className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 my-10 py-16 sm:py-32 md:py-48 md:flex-row md:space-x-4 md:text-left"
         ref={ref}
       >
-        <div className="md:mt-2 md:w-1/2" >
+        <div className="md:mt-2 md:w-2/5" >
           <Image
-            src="/pedro2.jpeg"
+            src="/pedro_2.jpg"
             alt=""
             width={325}
             height={200}
-            className="rounded-ss-full shadow-xl hover:opacity-70"
+            className="rounded-full border-4 shadow-xl hover:opacity-70"
           />
         </div>
         <div className="md:mt-2 md:w-3/5">
